e627b86f5e72bc8ad448ab9323c7e9f7eeb0584a
slayerblast <hery_75@hotmail.fr>
2022-06-21 10:48:48 +0200

    bing bong

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const app = require('./app');
 
 
-app.listen(process.env.PORT || 3000, '127.0.0.1', () => {
+app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
     console.log('Server écoute sur le port 3000')
 })
